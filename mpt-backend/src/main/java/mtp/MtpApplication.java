1def6914e8e62af3fce92223fd19bc0c210321c3
ukasz <lukas.matuszyk@gmail.com>
2019-11-21 17:53:35 +0100

    Dockerize backend and CORS configuration
    Add Docerfile and update pom.xml to build a docker image
    containing backend applicaction while executing maven package
    phase. Configure CORS policy to allow all origins(temporary).

--- mpt-backend/src/main/java/mtp/MtpApplication.java
+++ mpt-backend/src/main/java/mtp/MtpApplication.java
@@ -2,8 +2,10 @@ package mtp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "mtp.security.cors.hosts")
 public class MtpApplication {
     public static void main(String[] args) {
         SpringApplication.run(MtpApplication.class, args);
