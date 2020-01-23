1d394ec823062d9e0dc573ecb77995d4c2e28312
Krzysztof Wasilak <krzysztof.wasilak@gmail.com>
2020-01-23 20:30:55 +0100

    Some fixes

--- mpt-frontend/src/pages/HelpPage/HelpPage.jsx
+++ mpt-frontend/src/pages/HelpPage/HelpPage.jsx
@@ -7,7 +7,14 @@ import './HelpPage.scss';
 export function HelpPage() {
   return (
     <div className="help-page">
-      <div className="help-page__text">Prosimy o kontakt w kwestiach związanych z użytkowaniem naszego serwisu przy użyciu poniższego formularza. Będziemy szczęśliwi, aby pomóc wam w kwestii waszych zapytań.<br><br>Jeżeli potrzebujesz pomocy technicznej lub chcesz nawiązac współpracę proszę napisz wiadomość e-mail lub zadzwoń na podany poniżej numer telefonu.</div>
+      <div className="help-page__text">
+        Prosimy o kontakt w kwestiach związanych z użytkowaniem naszego serwisu przy użyciu
+        poniższego formularza. Będziemy szczęśliwi, aby pomóc wam w kwestii waszych zapytań.
+        <br />
+        <br />
+        Jeżeli potrzebujesz pomocy technicznej lub chcesz nawiązac współpracę proszę napisz
+        wiadomość e-mail lub zadzwoń na podany poniżej numer telefonu.
+      </div>
 
       <div className="help-page__container">
         <div className="help-page__box">
@@ -24,11 +31,16 @@ export function HelpPage() {
           <h2 className="help-page__contact-headline">Kontakt</h2>
 
           <p>
-            Email: influ-kontakt@influ.pl<br>
-			Telefon: 533 441 239<br>
-			Godziny pracy:<br>
-			poniedziałek - piątek 08:00 - 21:00<br>
-			sobota - niedziela 08:00 - 19:00<br>
+            Email: influ-kontakt@influ.pl
+            <br />
+            Telefon: 533 441 239
+            <br />
+            Godziny pracy:
+            <br />
+            poniedziałek - piątek 08:00 - 21:00
+            <br />
+            sobota - niedziela 08:00 - 19:00
+            <br />
           </p>
         </div>
       </div>
